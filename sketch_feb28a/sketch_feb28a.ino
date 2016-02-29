d9dd23744dce2229e7b3c1e1baff15ccb472e373
mastraa <mastraa@gmail.com>
2016-02-29 22:32:11 +0100

    Aggiornamenti
    sviluppo

--- sketch_feb28a/sketch_feb28a.ino
+++ sketch_feb28a/sketch_feb28a.ino
@@ -2,20 +2,31 @@
 float temp[6];
 byte * b = (byte *) &temp;
 
-byte buf[4];
+struct mvic { 
+byte tipo;
+float ext_t;
+float ext_u;
+float inl_t;
+float oul_t;
+float ins_t;
+}; 
+
+typedef struct mvic t_mvic;
+
+t_mvic Mvic;
+
+byte * c = (byte *) &Mvic;
 
 
 
 void setup() {
-  // put your setup code here, to run once:
-Serial.begin(9600);
-pinMode(13, OUTPUT);
-temp[0]=-11.75;
-temp[1]=10.40;
-temp[2]=89.10;
-temp[3]=-10.00;
-temp[4]=11.45;
-
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+  temp[0]=-11.75;
+  temp[1]=10.40;
+  temp[2]=89.10;
+  temp[3]=-10.00;
+  temp[4]=11.45;
 }
 
 void loop() {
@@ -23,18 +34,24 @@ void loop() {
   //Serial.write(array,4);
   //Serial.write(buf,4);
   //Serial.write('\n');
-  sendCommand(10, b, 20, '$', '\n'); 
+  sendCommand(10, b, 20, '$', '\n');
+  sendStruct(c, sizeof(mvic), '$', '\n'); 
   delay(1000);
-  byte i = 0;
-  while(Serial.available()&&i<4){
-     buf[i]  = Serial.read();
-     ++i;
+}
+
+
+void sendStruct(byte* commands, byte len, byte starter, byte ender){//send command
+  byte _XOR;
+  Serial.write(starter);
+  Serial.write(type);
+  //Serial.print(",");//separatore, per i byte non  serve
+  for (byte i=0; i<len; ++i){
+    Serial.write(commands[i]);
   }
-  buf[0]=10;
-  buf[3]=48;
-
-  // put your main code here, to run repeatedly:
-
+  Serial.write('*');
+  _XOR = type^getCheckSum(commands, len);
+  Serial.write(_XOR);
+  Serial.write(ender);
 }
 
 void sendCommand(byte type, byte* commands, byte len, byte starter, byte ender){//send command
