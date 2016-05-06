9d1baf7f5f2e4c6b3c4d5603a535b2f59ffb9023
luvemil <taran.marco@gmail.com>
2016-05-06 16:20:32 +0200

    Beautify the plot

--- libreria/myPlotWidget.py
+++ libreria/myPlotWidget.py
@@ -7,8 +7,11 @@ class myPlotWidget(pg.PlotWidget):
         super(myPlotWidget, self).__init__(*args,**kwargs)
         self.period = 3000
         self.semiInterval = 2500
+        self.thisPlotItem = self.getPlotItem()
 
     def tickHandler(self, tick):
-        x = np.arange(tick - self.semiInterval, tick + self.semiInterval,100) * np.pi / self.period
-        y = np.sin(x)
+        x = np.arange(tick - self.semiInterval, tick + self.semiInterval,250)
+        y = np.sin(x*np.pi/self.period)
+        x = x/1000. #put x in seconds
         self.plot(x,y, clear=True)
+        self.thisPlotItem.addLine(x=tick/1000.)
