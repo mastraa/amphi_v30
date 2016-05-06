02a3955fa4720a3ff162fd8849fd9e0b79f7dc22
luvemil <taran.marco@gmail.com>
2016-05-06 16:28:36 +0200

    Add documentation

--- libreria/myPlotWidget.py
+++ libreria/myPlotWidget.py
@@ -2,14 +2,24 @@ import pyqtgraph as pg
 import numpy as np
 
 class myPlotWidget(pg.PlotWidget):
-    """docstring for myPlotWidget"""
+    """Extend the class pyqtgraph.PlotWidget to serve our purposes.
+    TODO: add an argument `datafile` so that when the instance is created it
+    loads the data from a specific file, i.e. `fileStore/VELA012.TXT`.
+    """
     def __init__(self, *args, **kwargs):
         super(myPlotWidget, self).__init__(*args,**kwargs)
         self.period = 3000
         self.semiInterval = 2500
+        # We get the PlotItem associated in order to modify the plot, e.g
+        # add the vertical line in tickHandler.
         self.thisPlotItem = self.getPlotItem()
 
     def tickHandler(self, tick):
+        """
+        Connect this slot with a phonon.MediaObject.tick signal.
+        Handles the tick signal received from a phonon.MediaObject to keep
+        the plot in sync with the reproduction.
+        """
         x = np.arange(tick - self.semiInterval, tick + self.semiInterval,250)
         y = np.sin(x*np.pi/self.period)
         x = x/1000. #put x in seconds
